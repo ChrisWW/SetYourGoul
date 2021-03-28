e2fb07b6e90bc23e80ed0003550077b2b3bc8e92
Krystian Wiewior <krystian.wiewior@gmail.com>
2021-03-28 12:34:01 +0200

    Seperate fragments added

--- app/src/main/java/com/example/setyourgoal/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/setyourgoal/ui/main/SectionsPagerAdapter.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.setyourgoal.R
+import com.example.setyourgoal.fragments.Fragment1
+import com.example.setyourgoal.fragments.Fragment2
+import com.example.setyourgoal.fragments.Fragment3
 
 private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1,
-        R.string.tab_text_2,
-        R.string.tab_text_3
+    R.string.tab_text_1,
+    R.string.tab_text_2,
+    R.string.tab_text_3
 
 )
 
@@ -21,9 +24,13 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = Fragment1()
+            1 -> fragment = Fragment2()
+            2 -> fragment = Fragment3()
+        }
+        return fragment!!
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
